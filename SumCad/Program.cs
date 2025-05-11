2fbb8c5e7d5b29575a7cda8c95775527bb90b18f
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2025-05-11 10:30:28 -0500

    Initial Push

---
+++ SumCad/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SumCad.Application;
+
+using PicoGK;
+
+Application application = new Application();
+application.Run();
---
+++ SumCad/Source/Application.cs
@@ -0,0 +1,29 @@
+﻿namespace SumCad.Application;
+
+public class Application
+{
+    private GraphicsInstance graphicsInstance;
+    private UnlitRenderPipeline unlitRenderPipeline;
+    public Application()
+    {
+        graphicsInstance = new GraphicsInstance();
+        unlitRenderPipeline = new UnlitRenderPipeline(graphicsInstance);
+
+        graphicsInstance.OnInitialize += () =>
+        {
+            unlitRenderPipeline.Initialize();
+        };
+
+        graphicsInstance.OnRender += () =>
+        {
+            unlitRenderPipeline.Render();
+        };
+    }
+
+    public void Run()
+    {
+        graphicsInstance.CreateWindow(1000,700, "SumCad");
+        
+        graphicsInstance.Dispose();
+    }
+}
